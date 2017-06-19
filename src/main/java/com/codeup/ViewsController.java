ded2936fff7cbe9f607dec01093a369cfc17446c
Jaime Moncada <jaimemoncada13@gmail.com>
2017-06-19 16:53:38 -0500

    Started dice-roll exercise

--- src/main/java/com/codeup/ViewsController.java
+++ src/main/java/com/codeup/ViewsController.java
@@ -1,7 +1,9 @@
 package com.codeup;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ViewsController {
@@ -14,6 +16,17 @@ public class ViewsController {
     @GetMapping("/portfolio")
     public String portfolioPage() {
         return "portfolio";
+    }
+
+    @GetMapping("/roll-dice")
+    public String rollDice() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String rollDice(@PathVariable int n, Model model) {
+        model.addAttribute("n", n);
+        return "roll-dice";
     }
 
 }
